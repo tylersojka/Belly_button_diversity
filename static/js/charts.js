4c4f5192944dcf7c440a37074ec2da7a653719b1
tsojka <sojkasojkasojka@gmail.com>
2020-12-05 08:50:19 -0600

    added php file for heroku?

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,27 +1,85 @@
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-
+  var selector1 = d3.select("#dropOptions");
   // Use the list of sample names to populate the select options
   d3.json("static/js/samples.json").then((data) => {
     var sampleNames = data.names;
 
+    
+// old for each to append samples to selector tag for old dropdown
+    // sampleNames.forEach((sample) => {
+    //   selector
+    //     .append("option")
+    //     .text(sample)
+    //     .property("value", sample);
+    // });
+        
     sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
+  // jquery selector append a tag with the data value = to the sample id and a divider line
+      $('.dropdown-menu').append(`<a data-value="${sample}" class="dropdown-item">${sample}</a> <div class="dropdown-divider"></div>`)
+
     });
+// jquery selector function, on click of a number in dropdown bar, run the option changed function with the value selected.
+    $('.dropdown-menu a').click(function(){
+    let sampleValue = $(this).attr('data-value')
+    $("#navbarDropdown").text("Subject Number: " + $(this).attr('data-value'));
+    optionChanged(sampleValue)
+    });
+// for the search form, prevent reloading the page and set the search value = to the input and call option changed function
+    $(`#searchForm`).submit(function(e){
+      e.preventDefault();
+      let searchValue = $(`#searchInput`).val()
+      optionChanged(searchValue)
+      console.log(searchValue)
+    })
 
     // Use the first sample from the list to build the initial plots
     var firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
+    $("#navbarDropdown").text("Subject Number: " + firstSample);
   });
 }
 
 // Initialize the dashboard
 init();
+
+
+
+
+// hide buttons
+$("#option1").click(function(){
+  let barChart = $("#bar")
+  toggleStyle(barChart)
+  // $("#bar").toggle();
+});
+$("#option2").click(function(){
+  let gaugeChart = $("#gauge")
+  toggleStyle(gaugeChart)
+  // $("#gauge").toggle();
+});
+$("#option3").click(function(){
+  let bubbleChart = $("#bubble")
+  toggleStyle(bubbleChart)
+  // $("#bubble").toggle();
+});
+function removeStyle(chart){
+  chart.removeAttr("style")
+};
+
+function toggleStyle(chart) {
+  
+  if (chart.css("visibility") === "hidden") {
+    chart.css("visibility","visible");
+  } else {
+    chart.css("visibility", "hidden");
+  }
+}
+
+
+
+
 
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
@@ -29,6 +87,10 @@ function optionChanged(newSample) {
   buildCharts(newSample);
   
 }
+
+
+
+
 
 // Demographics Panel 
 function buildMetadata(sample) {
@@ -66,18 +128,18 @@ function buildCharts(sample) {
     var metaDataArray = metaData.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
     var result = resultArray[0];
-    console.log(result)
+    // console.log(result)
     metaDataSample= metaDataArray[0];
-    console.log(metaDataSample)
+    // console.log(metaDataSample)
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    wfreq = metaDataSample.wfreq;
-    ids = result.otu_ids;
-    labels = result.otu_labels;
-    values = result.sample_values;
-    console.log(wfreq)
-    console.log(ids.slice(0,10))
-    console.log(values.slice(0,10))
-    console.log(labels)
+    let wfreq = metaDataSample.wfreq;
+    let ids = result.otu_ids;
+    let labels = result.otu_labels;
+    let values = result.sample_values;
+    // console.log(wfreq)
+    // console.log(ids.slice(0,10))
+    // console.log(values.slice(0,10))
+    // console.log(labels)
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
@@ -91,10 +153,9 @@ function buildCharts(sample) {
 
     var yticks = ids.map(sampleObj => "OTU " + sampleObj).slice(0,10).reverse()
    
-
     // var yticks = ids.slice(0,10)
-    console.log(yticks)
-
+    // console.log(yticks)
+    
 
     
     // 8. Create the trace for the bar chart.
@@ -109,9 +170,17 @@ function buildCharts(sample) {
 
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-    title: "Top 10 Bacteria Cultures Found"
-    
-    
+    title: {
+      text: "Top 10 Bacteria Cultures Found",
+      font: {
+        color: "white"
+      }
+    },
+    plot_bgcolor: "333333",
+    paper_bgcolor: "333333",
+    font:{
+      color: "white"
+    }
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout)
@@ -126,15 +195,43 @@ function buildCharts(sample) {
     marker: {
       color: ids,
       size: values,
-      colorscale: "Portland"
+      colorscale: "Portland"},
     
-    }
+    
    
     }];
 
     // 2. Create the layout for the bubble chart.
     var bubbleLayout = {
-    title: "Bacteria Culters per Sample"
+    title: {
+      text: "Bacteria Culters per Sample",
+      font: {
+        color: "white"
+      }
+    },
+    plot_bgcolor: "333333",
+    paper_bgcolor: "333333",
+    yaxis: {
+      tickcolor: "grey",
+      tickwidth: 1,
+      
+      gridcolor: "grey",
+      gridwidth: .25,
+      
+      zerolinecolor: "grey",
+      zerolinewidth: 1,
+    },
+    xaxis: {
+      tickcolor: "grey",
+      tickwidth: .25,
+      title: "OTU ID",
+      gridcolor: "grey",
+      gridwidth: 1,      
+    },
+    font: {
+      color: "white"
+    }
+
     };
 
     // 3. Use Plotly to plot the data with the layout. 
@@ -143,7 +240,11 @@ function buildCharts(sample) {
 
     // 4. Create the trace for the gauge chart.
     var gaugeData = [{
-      title: {text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week"},
+      title: {text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week",
+                font: {
+                  color: "white"
+                }
+    },
       value: wfreq,
       type: "indicator",
       mode: "gauge+number",
@@ -161,7 +262,11 @@ function buildCharts(sample) {
     
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
-    
+      plot_bgcolor: "333333",
+      paper_bgcolor: "333333",
+      font: {
+        color: "white"
+      }
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
@@ -171,4 +276,5 @@ function buildCharts(sample) {
 
   });
 }
+
 
